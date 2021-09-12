85175585ae7f5f95e351f9e3118a58c87e490e59
aryav2 <vivek.arya2@mail.dcu.ie>
2021-09-12 12:29:50 +0100

    first commit

---
+++ src/main/java/com/vivekspringboot/dockerjenkinsgithubintegration/dockerjenkinsgithubintegration/DockerJenkinsGithubIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.vivekspringboot.dockerjenkinsgithubintegration.dockerjenkinsgithubintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsGithubIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsGithubIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivekspringboot/dockerjenkinsgithubintegration/dockerjenkinsgithubintegration/DockerJenkinsGithubIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekspringboot.dockerjenkinsgithubintegration.dockerjenkinsgithubintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsGithubIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
